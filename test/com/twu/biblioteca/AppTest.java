7f90ee0d7eb6d2b2700bdc4d2229001b0eec4fcc
Paul Venhaus <paul@ven.haus>
2019-08-09 16:55:40 +0200

    Add test for displaying list of books

--- test/com/twu/biblioteca/AppTest.java
+++ test/com/twu/biblioteca/AppTest.java
@@ -1,17 +1,19 @@
 package com.twu.biblioteca;
 
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Scanner;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-public class GreeterTest {
+public class AppTest {
 
     private final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
     private final PrintStream outOriginal = System.out;
@@ -34,5 +36,18 @@ public class GreeterTest {
         app.start();
 
         assertThat(outStream.toString(), is(greeting));
+    }
+
+    @Test
+    public void userCanViewListOfBooks() {
+        String[] listOfBooks = {"Book 1", "Book 2", "Book 3"};
+        BibliotecaApp app = new BibliotecaApp();
+
+        app.start();
+
+        String appOutput = outStream.toString();
+        String[] appOutputArr = appOutput.split("\n");
+        String[] result = Arrays.copyOfRange(appOutputArr, 1, 4);
+        assertThat(listOfBooks, is(result));
     }
 }
