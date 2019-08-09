624207c6e4306a6f0148f786345de33069830e2f
Paul Venhaus <paul@ven.haus>
2019-08-09 17:33:54 +0200

    Add view-only display of list of options

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -13,6 +13,8 @@ public class BibliotecaApp {
 
     public void start() {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        System.out.println("Choose an option by entering the associated number and pressing ENTER");
+        System.out.println("1. View a list of all books");
         for (String book : listOfBooks) {
             System.out.println(book);
         }
