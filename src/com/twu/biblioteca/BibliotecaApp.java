73d9e4aceb180a2a06544a10d76b1d2cb2cc0118
Paul Venhaus <paul@ven.haus>
2019-08-09 16:57:30 +0200

    Add automatic displat of list of books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,10 +2,15 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    String[] listOfBooks = {"Book 1", "Book 2", "Book 3"};
+
     public static void main(String[] args) {
     }
 
     public void start() {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        for (String book : listOfBooks) {
+            System.out.println(book);
+        }
     }
 }
